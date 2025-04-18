fb42548db7a363cdc18b720fc4b8426bb0d06bb4
Arriana Blais <arriblais@honeycomb.io>
2025-04-18 11:37:46 -0400

    add note about lack of testing

--- src/useNavigationInstrumentation.ts
+++ src/useNavigationInstrumentation.ts
@@ -2,6 +2,8 @@ import { usePathname } from 'expo-router';
 import { useEffect, useRef } from 'react';
 import { trace } from '@opentelemetry/api';
 
+// TODO: this code is only manually tested using a modified example app, automated tests should be added
+
 export default function useNavigationInstrumentation() {
   const routeNameRef = useRef<string>(undefined);
   const currentRoute = usePathname();
