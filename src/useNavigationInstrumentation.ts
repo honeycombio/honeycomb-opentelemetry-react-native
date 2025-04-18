1b59ea691cf99793d04fdb087b8c69269812de75
Arriana Blais <arriblais@honeycomb.io>
2025-04-18 10:40:21 -0400

    hook for expo router

---
+++ src/useNavigationInstrumentation.ts
@@ -0,0 +1,28 @@
+import { usePathname } from 'expo-router';
+import { useEffect, useRef } from 'react';
+import { trace } from '@opentelemetry/api';
+
+export default function useNavigationInstrumentation() {
+  const routeNameRef = useRef<string>(undefined);
+  const currentRoute = usePathname();
+
+  useEffect(() => {
+    const timestamp = Date.now();
+    const previousRoute = routeNameRef.current;
+    trace
+      .getTracer('io.honeycomb.navigation')
+      .startSpan('screen appeared', { startTime: timestamp })
+      .setAttribute('screen.name', currentRoute)
+      .end();
+
+    if (previousRoute) {
+      trace
+        .getTracer('io.honeycomb.navigation')
+        .startSpan('screen disappeared', { startTime: timestamp })
+        .setAttribute('screen.route', currentRoute)
+        .end();
+    }
+
+    routeNameRef.current = currentRoute;
+  }, [currentRoute]);
+}
