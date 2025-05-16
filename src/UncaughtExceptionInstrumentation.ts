b0031c577e354e62bd3ac1878036d879c2327e10
Arriana Blais <arriblais@honeycomb.io>
2025-05-16 11:39:38 -0400

    Global error handler autoinstrumentation

--- src/UncaughtExceptionInstrumentation.ts
+++ src/UncaughtExceptionInstrumentation.ts
@@ -1,4 +1,7 @@
-import { InstrumentationAbstract } from '@honeycombio/opentelemetry-web';
+import {
+  InstrumentationAbstract,
+  recordException,
+} from '@honeycombio/opentelemetry-web';
 import { type InstrumentationConfig } from '@opentelemetry/instrumentation';
 import type { Span } from '@opentelemetry/api';
 import { VERSION } from './version';
@@ -7,14 +10,18 @@ const LIBRARY_NAME = 'io.honeycomb.react-native.uncaught-exceptions';
 
 type ApplyCustomAttributesOnSpanFunction = (span: Span, error: Error) => void;
 
+// This is not exported by React Native so we need to decare it.
+type ErrorHandlerCallback = (error: any, isFatal?: boolean) => void;
+
 export interface UncaughtExceptionInstrumentationConfig
   extends InstrumentationConfig {
   applyCustomAttributesOnSpan?: ApplyCustomAttributesOnSpanFunction;
 }
 
 export class UncaughtExceptionInstrumentation extends InstrumentationAbstract {
-  //private _isEnabled: boolean;
+  private _isEnabled: boolean;
   readonly applyCustomAttributesOnSpan?: ApplyCustomAttributesOnSpanFunction;
+  private _oldErrorHandler?: ErrorHandlerCallback;
   constructor({
     enabled = true,
     applyCustomAttributesOnSpan,
@@ -30,15 +37,47 @@ export class UncaughtExceptionInstrumentation extends InstrumentationAbstract {
       this.enable();
     }
 
-    //this._isEnabled = enabled;
+    this._isEnabled = enabled;
     this.applyCustomAttributesOnSpan = applyCustomAttributesOnSpan;
   }
 
+  onError = (caughtError: any, isFatal?: boolean) => {
+    const error: Error = caughtError;
+
+    if (error) {
+      recordException(error, {}, this.tracer, this.applyCustomAttributesOnSpan);
+    }
+
+    if (this._oldErrorHandler) {
+      return this._oldErrorHandler(caughtError, isFatal);
+    }
+  };
+
+  init() {}
+
   enable(): void {
-    throw new Error('Method not implemented.');
+    if (this._isEnabled) {
+      this._diag.debug(`Instrumentation already enabled`);
+    }
+
+    this._isEnabled = true;
+    this._oldErrorHandler = ErrorUtils.getGlobalHandler();
+
+    ErrorUtils.setGlobalHandler(this.onError);
   }
-  disable(): void {}
-  protected init(): void {
-    throw new Error('Method not implemented.');
+
+  disable(): void {
+    if (!this._isEnabled) {
+      this._diag.debug(`Instrumentation already disabled`);
+    }
+
+    this._isEnabled = false;
+    if (this._oldErrorHandler) {
+      ErrorUtils.setGlobalHandler(this._oldErrorHandler);
+    }
+  }
+
+  public isEnabled() {
+    return this._isEnabled;
   }
 }
