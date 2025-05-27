e1884534237f2d0c4c09ac0f7c3d24a55b5e4ade
Arriana Blais <arriblais@honeycomb.io>
2025-05-27 13:04:40 -0400

    fix typedef

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,19 +2,30 @@ import {
   createStaticNavigation,
   useNavigationContainerRef,
   type ParamListBase,
+  type StaticParamList,
 } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MainScreen from './MainScreen';
 import { init as honeyInit } from './honeycomb';
 import { NavigationInstrumentation } from '@honeycombio/opentelemetry-react-native';
+import ErrorScreen from './ErrorScreen';
 
 honeyInit();
 
 const RootStack = createNativeStackNavigator({
   screens: {
     Main: MainScreen,
+    Errors: ErrorScreen,
   },
 });
+
+type RootStackParamList = StaticParamList<typeof RootStack>;
+
+declare global {
+  namespace ReactNavigation {
+    interface RootParamList extends RootStackParamList {}
+  }
+}
 
 const Navigation = createStaticNavigation(RootStack);
 
