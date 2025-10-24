18076729d05722a4256fa712d088ff63b4c41daa
Bee Klimt <beeklimt@honeycomb.io>
2025-10-24 11:19:50 -0700

    fix: remove broken plugin code that writes a text file (#63)
    ## Which problem is this PR solving?
    
    The UUID plugin currently generates a text file at
    `./dist/tmp/hny/SourceMapUuid.txt` whenever it's loaded. But that's not
    how react plugins are supposed to work. The iOS and Android plugins only
    generate new UUIDs during actual pre-builds. So, often the UUID in the
    text file doesn't match the one in the code. Using it would cause
    problems. And using the ones from iOS and Android directly isn't that
    difficult.
    
    ## Short description of the changes
    
    This just removes the text file generation from the plugin. Making the
    plugin work correctly would be pretty painful.
    
    ## How to verify that this has the expected result
    
    Tested manually.
    
    ---
    
    - [X] CHANGELOG is updated
    N/A ~- [ ] README is updated with documentation~

--- src/plugin/withUUIDPlugin.ts
+++ src/plugin/withUUIDPlugin.ts
@@ -1,6 +1,5 @@
 import type { ConfigPlugin } from '@expo/config-plugins';
 
-import fs from 'node:fs';
 import withAndroidPlugin from './withUUIDAndroidPlugin';
 import withIOSPlugin from './withUUIDIosPlugin';
 
@@ -9,14 +8,9 @@ const withPlugin: ConfigPlugin = (config) => {
   // Apply Android modifications first
   config = withAndroidPlugin(config, { sourceMapUuid });
   // Then apply iOS modifications and return
-  const resultConfig = withIOSPlugin(config, { sourceMapUuid });
+  config = withIOSPlugin(config, { sourceMapUuid });
 
-  if (!fs.existsSync('./dist/tmp/hny')) {
-    fs.mkdirSync('./dist/tmp/hny', { recursive: true });
-  }
-  fs.writeFileSync('./dist/tmp/hny/SourceMapUuid.txt', sourceMapUuid);
-
-  return resultConfig;
+  return config;
 };
 
 export default withPlugin;
